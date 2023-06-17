3f67189216a9208e45d96467a2ace1bd915723a8
Twinkle <twinklenarnaware32@gmail.com>
2023-06-17 22:27:26 +0530

    comment-1

--- src/main/java/com/driver/services/ParkingLotService.java
+++ src/main/java/com/driver/services/ParkingLotService.java
@@ -1,7 +1,12 @@
 package com.driver.services;
 
+import com.driver.model.ParkingLot;
+import com.driver.model.Spot;
+import org.springframework.stereotype.Service;
+
+@Service
 public interface ParkingLotService {
-    
+
     void deleteSpot(int spotId);
 
     Spot updateSpot(int parkingLotId, int spotId, int pricePerHour);
